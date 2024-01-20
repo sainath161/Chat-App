373f1310064e3b09de6b71223e0e49612a2a632f
Sainath <71361447+sainath161@users.noreply.github.com>
2024-01-20 12:57:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
     cors: { 
-        origin: 'http://127.0.0.1:5500',
+        origin: 'https://chat-app-six-fawn.vercel.app/',
     },
 });
 
